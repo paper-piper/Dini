ae6775d13212ec9e420bbb965a1c1487626e3544
paper-piper <113584610+paper-piper@users.noreply.github.com>
2024-11-18 15:31:19 +0200

    updated bootstrap tests

--- Bootstrap/bootstrap1.py
+++ Bootstrap/bootstrap1.py
@@ -10,18 +10,21 @@ if __name__ == "__main__":
         ("127.0.0.1", 9090): None  # Sample peer to be shared with Bootstrap2
     }
 
+    # Use localhost for same-computer testing
+    bootstrap_ip = "127.0.0.1"
+
     # Initialize Bootstrap1 with peer_connections
     bootstrap1 = Bootstrap(
         is_bootstrap=True,
         port=8080,
-        peer_connections=initial_peers
+        peer_connections=initial_peers,
+        ip=bootstrap_ip
     )
-
     # Keep the script running
     stop_event = Event()
 
     try:
         print("Bootstrap1 is running. Waiting for communication...")
-        #stop_event.wait()
+        stop_event.wait()
     except KeyboardInterrupt:
         print("Bootstrap1 shutting down...")
--- Bootstrap/bootstrap2.py
+++ Bootstrap/bootstrap2.py
@@ -6,13 +6,17 @@ logger = setup_logger("bootstrap2")
 
 
 if __name__ == "__main__":
+
+    # Use localhost for same-computer testing
+    bootstrap_ip = "127.0.0.1"
+
     # Initialize Bootstrap2 without peer_connections
     bootstrap2 = Bootstrap(
         is_bootstrap=False,
-        port=9090
+        port=9090,
+        ip=bootstrap_ip
     )
-
-
+    logger.info(f"tried to connect to peers, got the addresses:  {bootstrap2.peer_connections}")
     # Keep the script running
     stop_event = Event()
 
