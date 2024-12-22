0d66bbfa83a85d506fa337022871a7a6943629d9
paper-piper <113584610+paper-piper@users.noreply.github.com>
2024-12-22 13:26:27 +0200

    made user automatically save wallet at end of init

--- network/user.py
+++ network/user.py
@@ -36,11 +36,13 @@ class User(Bootstrap):
         self.wallet_filename = FilesSettings.WALLET_FILE_NAME if wallet_filename is None else wallet_filename
         self.wallet = wallet if wallet else self.load_wallet()
         # try and get updates for wallet (in case of missing out)
+
         self.send_distributed_message(
             MsgTypes.REQUEST_OBJECT,
             MsgSubTypes.BLOCKCHAIN,
             self.wallet.latest_hash
         )
+        self.save_wallet()
 
     def __del__(self):
         if self.port_manager:
